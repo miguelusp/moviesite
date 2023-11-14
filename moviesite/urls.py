5aa5501697fbaadbf5c8d9515ee20c6517ea0304
Miguel Figueiras <miguelpassosfigueiras@usp.br>
2023-11-14 03:12:14 -0300

    Inserir listagem, busca e criação de filmes

--- moviesite/urls.py
+++ moviesite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-
+ 
 urlpatterns = [
-    path('', include('staticpages.urls')), # modifique esta linha
+    path('', include('staticpages.urls')),
+    path('movies/', include('movies.urls')), # adicionar esta linha
     path('admin/', admin.site.urls),
-]
+]   
